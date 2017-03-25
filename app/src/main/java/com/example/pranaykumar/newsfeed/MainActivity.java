12e57fcf8619c2e5631dcea1c4133b305c679a84
Anugurti Pranaykumar <pranaypandu.225@gmail.com>
2017-03-25 12:27:42 +0530

    Network Connection Test and Progress bar

--- app/src/main/java/com/example/pranaykumar/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/pranaykumar/newsfeed/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.pranaykumar.newsfeed;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +28,16 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     private NewsAdapter mAdapter;
     private TextView mEmptyStateTextView;
+    private TextView mTextView;
+    private View root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView newsListView = (ListView) findViewById(R.id.list);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_list);
+        mTextView=(TextView)findViewById(R.id.header);
+        root=findViewById(R.id.activity_main);
         mAdapter = new NewsAdapter(this, new ArrayList<News>());
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -47,9 +58,29 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
                 startActivity(websiteIntent);
             }
         });
-        LoaderManager loader=getLoaderManager();
-        loader.initLoader(1,null,this);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Get details on the currently active default data network
+        NetworkInfo networkInfo=connMgr.getActiveNetworkInfo();
+
+        //if there is network connection,fetch data
+        if(networkInfo!=null&&networkInfo.isConnected())
+        {
+            LoaderManager loader=getLoaderManager();
+            loader.initLoader(1,null,this);
+        }
+        else{
+            View loading_indicator=(ProgressBar)findViewById(R.id.loading_indicator);
+            loading_indicator.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+            mTextView.setText("");
+
+
+        }
     }
+
 
         @Override
         public Loader<List<News>> onCreateLoader(int id, Bundle args) {
@@ -59,13 +90,20 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
         @Override
         public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+            View loading_indicator=(ProgressBar)findViewById(R.id.loading_indicator);
+            loading_indicator.setVisibility(View.GONE);
+
             mAdapter.clear();
             Log.i(LOG_TAG,"in onLoadFinished");
             if(data!=null&&!data.isEmpty()){
-
                 mAdapter.addAll(data);
                 Log.i(LOG_TAG,"data added");
             }
+            else {
+                mEmptyStateTextView.setText(R.string.no_news);
+            }
+
+
         }
 
         @Override
